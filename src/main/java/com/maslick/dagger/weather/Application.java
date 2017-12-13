eb761edf46dc22f66b7f0a2333136e83d6855ab4
Pavel Maslov <pavel.maslov@ijs.si>
2017-12-13 14:54:13 +0100

    what would it look like without dagger

--- src/main/java/com/maslick/dagger/weather/Application.java
+++ src/main/java/com/maslick/dagger/weather/Application.java
@@ -6,10 +6,16 @@ import com.maslick.dagger.weather.modules.RosgidrometModule;
 
 public class Application {
     public static void main(String args[]) {
+
+        // WebSocket ws = new WebSocket();
+        // WeatherService rosgidromet = new Rosgidromet(ws, "1234567890");
+        // GpsSensor gps = new GpsSensor();
+        // LocationManager locationManager = new LocationManager(gps);
+        // WeatherReporter reporter = new WeatherReporter(rosgidromet, locationManager);
+        // reporter.report();
+
         RosgidrometModule rosgidromet = new RosgidrometModule("1234567890");
-        AppComponent component = DaggerAppComponent.builder()
-                .rosgidrometModule(rosgidromet)
-                .build();
+        AppComponent component = DaggerAppComponent.builder().rosgidrometModule(rosgidromet).build();
         WeatherReporter reporter = component.getWeatherReporter();
         reporter.report();
     }
